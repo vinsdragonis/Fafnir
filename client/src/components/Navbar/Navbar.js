fcfdbbd395daad85674dd4be5fb48556f33cd53b
vinsdragonis <zrexteam128@gmail.com>
2021-11-01 19:55:19 +0530

    Changed background and text colors

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <nav style={{display: "flex", justifyContent: "space-between"}}>
       <div className="tr" style={{display: "flex", justifyContent: "space-between", paddingLeft: "50px"}}>
-        <p className="f4 white link dim underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>About us</p>
-        <p className="f4 white link dim underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>Portfolio</p>
-        <p className="f4 white link dim underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>Contact</p>
+        <p className="f4 black link dim underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>About us</p>
+        <p className="f4 black link dim underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>Portfolio</p>
+        <p className="f4 black link dim underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>Contact</p>
       </div>
     </nav>
   );
