871cd392da79b4bd6f3e1f07844468421774a86e
vinsdragonis <zrexteam128@gmail.com>
2021-11-01 13:54:51 +0530

    Added logo and modded navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import EditorComp from './components/Editor/EditorComp';
 import './App.css';
+import Logo from './components/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='pa3 br3 shadow-5' style={{display: "flex", justifyContent: "flex-start"}}>
+        <div
+          className='pa1 br3'
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            backgroundColor: "black",
+            borderRadius: "0",
+            paddingTop: "10px"
+          }}
+        >
+          <Logo />
           <Navbar />
         </div>
       </header>
