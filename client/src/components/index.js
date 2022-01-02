eb93abecd3abc940e88fc5efdf360f1efdb318e7
vinsdragonis <zrexteam128@gmail.com>
2022-01-02 21:27:39 +0530

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,6 +6,7 @@ export { default as TeamChannelPreview } from './TeamChannelPreview';
 export { default as ChannelInner } from './ChannelInner';
 export { default as CreateChannel } from './CreateChannel';
 export { default as EditChannel } from './EditChannel';
+export { default as ChannelInvite } from './ChannelInvite';
 export { default as TeamMessage } from './TeamMessage';
 export { default as Auth } from './Auth';
 export { default as UserList } from './UserList';
