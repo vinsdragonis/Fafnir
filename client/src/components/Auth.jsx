2c47b737dbb79eca8225dc67e28698cf2fe3256a
vinsdragonis <zrexteam128@gmail.com>
2022-01-12 21:58:16 +0530

    .

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -30,10 +30,11 @@ const Auth = () => {
         const {  username, password, phoneNumber, avatarURL } = form;
 
         const URL = 'http://localhost:5000/auth';
+        // const URL = 'https://medical-pager.herokuapp.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName,} } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
-        });
+        }, { mode: 'cors' });
 
         cookies.set('token', token);
         cookies.set('username', username);
--- client/src/components/ChannelInvite.jsx
+++ client/src/components/ChannelInvite.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { useChatContext } from 'stream-chat-react';
-import { UserList } from './';
-import { CloseCreateChannel } from '../assets';
+// import React, { useState } from 'react';
+// import { useChatContext } from 'stream-chat-react';
+// import { UserList } from './';
+// import { CloseCreateChannel } from '../assets';
 
-export default function ChannelInvite() {
-    return (
-        <div>
+// export default function ChannelInvite() {
+//     return (
+//         <div>
             
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
